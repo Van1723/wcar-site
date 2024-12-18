c00347951d6b86a988802fc1ee3f14ed7774f84b
Vanessa Silva <vanessa.tecma@gmail.com>
2024-12-18 10:12:10 -0300

    validations_forms e others

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,12 +2,12 @@ function abrirWhatsapp(){
   document.getElementById("formSubmit").onsubmit;
   var Nome = document.getElementById("nome").value; 
   var Celular = document.getElementById("celular").value;
-  var Problema  = document.getElementById("problema").value; // e assim vai
+  var Problema  = document.getElementById("problema").value; 
   var marcaModelo = document.getElementById("marcaModelo").value;
   var Descricao = document.getElementById("descricao").value; 
   var mensagem = "Celular: " + Celular  +  "\n Problema: "+ Problema +" "+ "\n Marca e modelo: " + " "
-  + marcaModelo +" "+ "\n Descrição: "+ " "+ Descricao; // Adicione outros campos conforme necessário  
-  const phoneNumber = "5518997125999"; // Substitua pelo número desejado
+  + marcaModelo +" "+ "\n Descrição: "+ " "+ Descricao;  
+  const phoneNumber = "5518997125999"; // Substitua pelo número necessario
 
 
   console.log(mensagem);
