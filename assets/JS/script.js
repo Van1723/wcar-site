5daa6da3ed4013f6064d3677df1f3946eb411565
Vanessa Silva <vanessa.tecma@gmail.com>
2024-12-19 08:21:28 -0300

    final changes

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -7,12 +7,12 @@ function abrirWhatsapp(){
   var Descricao = document.getElementById("descricao").value; 
   var mensagem = "Celular: " + Celular  +  "\n Problema: "+ Problema +" "+ "\n Marca e modelo: " + " "
   + marcaModelo +" "+ "\n Descrição: "+ " "+ Descricao;  
-  const phoneNumber = "5518997125999"; // Substitua pelo número necessario
+  const phoneNumber = "5518997973662"; // Substitua pelo número necessario
 
 
   console.log(mensagem);
   // Formata a mensagem para a URL
-  const whatsappMessage = `Olá, meu nome é ${encodeURIComponent(Nome)} ${encodeURIComponent(mensagem)}`;
+  const whatsappMessage = `Olá, vim do site!  Meu nome é ${encodeURIComponent(Nome)} ${encodeURIComponent(mensagem)}`;
 
   // Monta o link do WhatsApp
   const whatsappURL = `https://wa.me/${phoneNumber}?text=${whatsappMessage}`;
